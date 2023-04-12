a2e020dc2a8bf4b253f14f0ebd3d9cd9827b630a
wangyongjie <3033784564@qq.com>
2023-04-12 11:29:23 +0800

    前端提交

---
+++ user-center-frontend-master/src/constants/index.ts
@@ -0,0 +1,6 @@
+export const SYSTEM_LOGO = "https://pic.code-nav.cn/user_avatar/1637737736167280641/aC6GyMMY-YZH]DL4(S5E$70ZEXHPE9DU.png";
+
+/**
+ * 知识星球文档地址
+ */
+export const PLANET_LINK = "https://docs.qq.com/doc/DUG93dVNHbVZjZXpo";
